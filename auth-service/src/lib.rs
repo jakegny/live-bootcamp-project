4137a80ce6553b5a627e8cfe753525a43542646c
Jake Nylund <jakegny@gmail.com>
2024-07-28 05:49:16 -0700

    S2T1 - WIP

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -2,7 +2,9 @@ use axum::{routing::post, serve::Serve, Router};
 use std::error::Error;
 use tower_http::services::ServeDir;
 
+pub mod domain;
 pub mod routes;
+pub mod services;
 
 // This struct encapsulates our application-related logic.
 pub struct Application {
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -22,3 +22,27 @@ async fn should_return_422_if_malformed_input() {
         );
     }
 }
+
+#[tokio::test]
+async fn should_return_201_if_valid_input() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    // TODO: add more malformed input test cases
+    let test_cases = [serde_json::json!({
+                "email": random_email,
+        "password": "password123",
+        "requires2FA": true
+    })];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case); // call ``
+        assert_eq!(
+            response.await.status().as_u16(),
+            201,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
+}
